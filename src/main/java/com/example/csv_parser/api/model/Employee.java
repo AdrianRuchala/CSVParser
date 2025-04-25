c2a45a986b4b6ea1f608611dcb8d0f441ba6a4a2
AdrianRuchala <adrianruchala03@gmail.com>
2025-04-25 10:51:48 +0200

    Added Employee entity for database mapping

---
+++ src/main/java/com/example/csv_parser/api/model/Employee.java
@@ -0,0 +1,73 @@
+package com.example.csv_parser.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    private String employeeCode;
+    private String firstName;
+    private String lastName;
+    private String position;
+    private String department;
+    private String email;
+    private String phone;
+
+    public Employee() {
+    }
+
+    public Employee(String employeeCode, String firstName, String lastName, String position, String department, String email, String phone) {
+        this.employeeCode = employeeCode;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.department = department;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee that = (Employee) o;
+        return Objects.equals(employeeCode, that.employeeCode) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(position, that.position) && Objects.equals(department, that.department) && Objects.equals(email, that.email) && Objects.equals(phone, that.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(employeeCode, firstName, lastName, position, department, email, phone);
+    }
+}
